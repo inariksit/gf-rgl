18a38eefcdc88339d5fdfc61377d8ec5fe509ca2
Inari Listenmaa <inari.listenmaa@gmail.com>
2020-03-06 12:03:19 +0100

    Add types and T.pack

--- src/polish/format.zsh
+++ src/polish/format.zsh
@@ -7,7 +7,7 @@ data Case = Sg_Nom | Sg_Gen | Sg_Dat | Sg_Acc | Sg_Ins | Sg_Loc | Sg_Voc
               | Pl_Nom | Pl_Gen | Pl_Dat | Pl_Acc | Pl_Ins | Pl_Loc | Pl_Voc
       deriving (Show, Eq, Ord, Enum)
 
-paradigms = [
+paradigms :: [((T.Text, Int), [(Case, T.Text)])]
 "
 # First two seds: preparation for irregularities
 grep -A16 "oper.*lexem" NounMorphoPol.gf \
@@ -15,7 +15,7 @@ grep -A16 "oper.*lexem" NounMorphoPol.gf \
 | sed 's/      let x = lexem in/      let x = Predef.tk 0 lexem in/' \
 | sed 's/    table { //g' | tr -s '\n' \
 \
-| sed -E 's/.* (mkNTable.....?) .*/    \(\("\1", /g' \
+| sed -E 's/.* (mkNTable.....?) .*/    \(\(T.pack "\1", /g' \
 | sed -E 's/    oper (.*) lexem.*/\1/g' \
 | sed -E 's/      let x = Predef.tk (.*) lexem in/                   \1\), [/g' \
 | sed -E 's/      SF (..) (...).*(".*");/        (\1_\2, T.pack \3),/g' \
